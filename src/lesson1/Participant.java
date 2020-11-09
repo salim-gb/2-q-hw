61077d5ec6315d14867685cdea1adfdbcebc4855
Salim <salimalsado.gb@gmail.com>
2020-11-09 17:50:35 +0300

    2-q-hw1

---
+++ src/lesson1/Participant.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public interface Participant {
+    void run(int distance);
+    void jump(int height);
+    boolean isOnDistance();
+    void info();
+}
---
+++ src/lesson1/Robot.java
@@ -0,0 +1,45 @@
+package lesson1;
+
+public class Robot implements Participant{
+    private String name;
+    private int maxRun;
+    private int maxJump;
+    private boolean onDistance;
+
+    public Robot(String name, int maxRun, int maxJump) {
+        this.name = name;
+        this.maxRun = maxRun;
+        this.maxJump = maxJump;
+        this.onDistance = true;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (maxRun <= distance) {
+            System.out.println("Участник " + name + " не мог добежать " + distance);
+            onDistance = false;
+        } else {
+            System.out.println("Участник " + name + " пробежал " + distance);
+        }
+    }
+
+    @Override
+    public void jump(int height) {
+        if (maxRun <= height) {
+            System.out.println("Участник " + name + " не мог брыгнуть " + height);
+            onDistance = false;
+        } else {
+            System.out.println("Участник " + name + " прыгнул " + height);
+        }
+    }
+
+    @Override
+    public boolean isOnDistance() {
+        return onDistance;
+    }
+
+    @Override
+    public void info() {
+        System.out.printf("%s %b \n", name, onDistance);
+    }
+}
